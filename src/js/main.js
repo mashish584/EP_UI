25ac8dc598db6e4b8c4e2232d96d8becde7a4685
Ashish Mehra <sdashish.am@gmail.com>
2019-02-15 22:44:26 +0530

    Cards Element created for homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { catchClickEvent } from "./js-utility";
 import { dropdownList } from "./drop-down-ui.js";
+import { navDrop } from "./nav-drop";
 
 // function calls
 catchClickEvent();
 dropdownList();
+navDrop();
