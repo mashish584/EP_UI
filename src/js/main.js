29183900b909535881b077a6167c7baedf418d77
Ashish Mehra <sdashish.am@gmail.com>
2019-02-26 15:02:31 +0530

    ADD EVENT UI FORM - SCSS ~ Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import { catchClickEvent } from "./js-utility";
 import { dropdownList } from "./drop-down-ui.js";
 import { navDrop } from "./nav-drop";
+import { inputPicker } from "./picker-ui";
 
 // function calls
 catchClickEvent();
 dropdownList();
 navDrop();
+inputPicker();
